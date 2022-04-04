2de70759106b5c41243d43015d59d9f1e142e68f
toboozy <91456563+toboozy@users.noreply.github.com>
2022-04-04 16:33:00 +1000

    Movement script

---
+++ Dream Catcher/Assets/Scripts/Movement.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    [SerializeField] float moveSpeed = 4;
+    //canMove is set to true if move timer is < 1
+    public bool canMove = true;
+    //Move timer determines if we can move. If it's above 0, we cannot. Used by hazards to slow us down.
+    public float moveTimer = 0; 
+
+    private void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void Update()
+    {
+
+        MoveTimer();
+
+    }
+
+    //This moves the object in the intended direction based on their move speed. Called by the input script of the object
+    public void MoveObject(Vector2 direction)
+    {
+        if (canMove)
+        {
+            rb.velocity = direction * moveSpeed * Time.deltaTime;
+        }
+    }
+
+    void MoveTimer()
+    {
+
+        if (moveTimer > 0)
+        {
+
+            canMove = false;
+            //Ensures that it doesn't go below 0 whilst ticking down by 1 each second
+            moveTimer = Mathf.Clamp(moveTimer, 0, 1 * Time.deltaTime);
+
+        }
+        else
+        {
+
+            canMove = true; 
+
+        }
+
+    }
+
+}
